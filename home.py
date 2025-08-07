4c7860c220b42dfcbfb20232c3d2b32fd8cfc384
Samkwt <Samkwt2003@gmail.com>
2025-08-06 21:27:43 -0500

    Initial commit of RAG chatbot project

---
+++ home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Welcome to my first AI Chatbot")
+
+st.divider()
+
+st.subheader("Interact with different versions of the chatbot from the sidebar")
+
+st.image("https://th.bing.com/th/id/OIP.aShmGxZv82Ee6jJ8NJ_WkwHaJn?rs=1&pid=ImgDetMain")
+
+st.write("Built with love <3 by Samkwt")
